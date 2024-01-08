58389d08353aa6f77cb399cf1e022c9dfcbd9635
vitor <vitorcastanhos@gmail.com>
2024-01-08 20:43:38 -0300

    adding API scrollReveal, buttonBaclTotop which included some functions and styles for some of them

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,30 @@
+window.addEventListener("scroll", onscroll);
+
+function onscroll() {
+   showBackToTopButtonOnScroll();
+}
+
 function openMenu() {
-  const sidebar = document.querySelector(".sidebar")
-  sidebar.style.display = "flex"
+   const sidebar = document.querySelector(".sidebar");
+   sidebar.style.display = "flex";
 }
 
 function closeMenu() {
-  const sidebar = document.querySelector(".sidebar")
-  sidebar.style.display = "none"
+   const sidebar = document.querySelector(".sidebar");
+   sidebar.style.display = "none";
 }
+
+function showBackToTopButtonOnScroll() {
+   if (scrollY > window.innerHeight - 100) {
+      backToTopButton.classList.add("show");
+   } else {
+      backToTopButton.classList.remove("show");
+   }
+}
+
+ScrollReveal({
+   origin: "top",
+   distance: "40px",
+   duration: "900",
+   reset: true,
+}).reveal(`#home, #home h1, #home p, #home button, #home img, #services, #services h2, #services img, #services h3, #services p, #gallery, #gallery h2, #gallery img, #message, #banner, #contact, #contact label, #contact input, #contact textarea, #contact button, #footer`);
