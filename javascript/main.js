29ce3ca1766ad65f087c0a170fd3c7e902b30470
vitor <vitorcastanhos@gmail.com>
2024-01-09 07:51:49 -0300

    few changes on html, deleting some img and svgs, css styles and scrollreveal

--- javascript/main.js
+++ javascript/main.js
@@ -24,7 +24,9 @@ function showBackToTopButtonOnScroll() {
 
 ScrollReveal({
    origin: "top",
-   distance: "40px",
+   distance: "30px",
    duration: "900",
    reset: true,
-}).reveal(`#home, #home h1, #home p, #home button, #home img, #services, #services h2, #services img, #services h3, #services p, #gallery, #gallery h2, #gallery img, #message, #banner, #contact, #contact label, #contact input, #contact textarea, #contact button, #footer`);
+}).reveal(
+   `#home, #home h1, #home p, #home button, #home .imgElement img, #services, #services h2, #services img, #services h3, #services p, #gallery, #gallery h2, #gallery img, #message, #banner, #contact, #contact label, #contact input, #contact textarea, #contact button, #footer`
+);
