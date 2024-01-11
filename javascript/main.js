320c2b872e0ed199bb3ec2ea298f0b805451a5d8
vitor <vitorcastanhos@gmail.com>
2024-01-10 21:48:02 -0300

    adding some @media content to desktop version

--- javascript/main.js
+++ javascript/main.js
@@ -25,7 +25,7 @@ function showBackToTopButtonOnScroll() {
 ScrollReveal({
    origin: "top",
    distance: "30px",
-   duration: "900",
+   duration: "600",
    reset: true,
 }).reveal(
    `#home, #home h1, #home p, #home button, #home .imgElement img, #services, #services h2, #services img, #services h3, #services p, #gallery, #gallery h2, #gallery img, #message, #banner, #contact, #contact label, #contact input, #contact textarea, #contact button, #footer`
